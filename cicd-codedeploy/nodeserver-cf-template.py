8cafaab0192c5340015c52bbe120cb79b3c86142
PB <lindynetech@gmail.com>
2021-03-20 03:49:59 -0400

    codedeploy agent on nodeserver

---
+++ cicd-codedeploy/nodeserver-cf-template.py
@@ -0,0 +1,161 @@
+"""Generating CloudFormation template."""
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
+
+from troposphere.iam import (
+  InstanceProfile,
+  PolicyType as IAMPolicy,
+  Role,
+)
+from awacs.aws import (
+  Action,
+  Allow,
+  Policy,
+  Principal,
+  Statement,
+)
+
+from awacs.sts import AssumeRole
+
+ApplicationName = "nodeserver" 
+ApplicationPort = "3000" 
+PublicCidrIp = "0.0.0.0/0"
+amiId = "ami-038f1ca1bd58a5790"
+
+GithubAccount = "lindynetech"
+GithubAnsibleURL = "https://github.com/{}/effectivedevops/".format(GithubAccount)
+
+AnsiblePullCmd = \
+    "/usr/bin/ansible-pull -U {} ansible/{}.yml -i localhost".format(
+        GithubAnsibleURL,
+        ApplicationName
+    )
+
+t = Template()
+
+t.add_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
+    "KeyPair",
+    Description="Name of an existing EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    Default = "sysops-key",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp=PublicCidrIp,
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+# add codedeploy agent
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "yum update -y",
+    "yum install -y git python2-pip ruby wget",
+    "wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install",
+    "chmod +x ./install",
+    "./install auto",
+    "pip install --upgrade pip",
+    "pip install ansible",
+    "amazon-linux-extras install epel",
+    AnsiblePullCmd,
+    "echo '*/10 * * * * {}' > /etc/cron.d/ansible-pull".format(AnsiblePullCmd)
+]))
+
+t.add_resource(Role(
+    "Role",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["ec2.amazonaws.com"])
+            )
+        ]
+    )
+))
+
+t.add_resource(InstanceProfile(
+    "InstanceProfile",
+    Path="/",
+    Roles=[Ref("Role")]
+))
+
+# t.add_resource(IAMPolicy(
+#     "Policy",
+#     PolicyName="AllowCodePipeline",
+#     PolicyDocument=Policy(
+#         Statement=[
+#             Statement(
+#                 Effect=Allow,
+#                 Action=[Action("codepipeline", "*")],
+#                 Resource=["*"]
+#             )
+#         ]
+#     ),
+#     Roles=[Ref("Role")]
+# ))
+
+t.add_resource(IAMPolicy(
+    "Policy",
+    PolicyName="AllowS3",
+    PolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[Action("s3", "*")],
+                Resource=["*"])
+        ]
+    ),
+    Roles=[Ref("Role")]
+))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId=amiId,
+    InstanceType="t2.micro",
+    SecurityGroups=[Ref("SecurityGroup")],
+    KeyName=Ref("KeyPair"),
+    UserData=ud,
+    IamInstanceProfile=Ref("InstanceProfile"),
+))
+
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance.",
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
+
+print(t.to_yaml())
